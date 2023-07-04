f331cdd66f8c50713ea7a2b54194e0bcf512bb6b
Fatih Yanar <fatih.yanar@mni.thm.de>
2023-07-04 10:39:17 +0200

    Initial Commit

---
+++ HandlerApp/HandlerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandlerAppApp.swift
+//  HandlerApp
+//
+//  Created by ii on 04.07.23.
+//
+
+import SwiftUI
+
+@main
+struct HandlerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
